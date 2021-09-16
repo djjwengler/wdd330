b37e387c7331127db66f188a0fb74dff9daf673f
Jaz Wengler <djjwengler@hotmail.com>
2021-09-16 17:42:59 -0400

    finished notes, fixed errors

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ for (let i=0; i<links.length; i++){
 let li = document.createElement('li');
 let label = links[i].label;
 let url = links[i].url;
-li.innerHTML = "<a href='${ url }'>" + label + "</a>";
+li.innerHTML = "<a href='" + url + "'>" + label + "</a>";
 list.appendChild(li);
 }
 
