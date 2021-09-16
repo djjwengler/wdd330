75d41cdf1a88f74cf0a2b9358253e874ae0b799a
Jaz Wengler <djjwengler@hotmail.com>
2021-09-16 15:30:11 -0400

    portfolio js

---
+++ main.js
@@ -0,0 +1,15 @@
+const links = [
+    {label: "Week1 notes",
+    url: "week1/index.html"}
+]
+
+let list = document.getElementById('list');
+
+for (let i=0; i<links.length; i++){
+let li = document.createElement('li');
+let label = links[i].label;
+let url = links[i].url;
+li.innerHTML = "<a href='${ url }'>" + label + "</a>";
+list.appendChild(li);
+}
+
