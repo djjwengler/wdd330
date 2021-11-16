a061efe24df0bc06db4513b6020f70bd4f1d8617
Jaz Wengler <djjwengler@hotmail.com>
2021-11-15 19:52:39 -0500

    remove node modules

--- week10/quakes/utilities.js
+++ week10/quakes/utilities.js
@@ -1,4 +1,4 @@
-function getJSON(url) {
+export function getJSON(url) {
     return fetch(url)
         .then(function(response) {
             if (!response.ok) {
@@ -13,7 +13,7 @@ function getJSON(url) {
 }
 
 
-const getLocation = function(options) {
+export const getLocation = function(options) {
     return new Promise(function(resolve, reject) {
         navigator.geolocation.getCurrentPosition(resolve, reject, options);
     });
