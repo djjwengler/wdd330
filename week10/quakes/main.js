4924f904b8bbb37da6df2b3c9cac13a75c410b13
Jaz Wengler <djjwengler@hotmail.com>
2021-11-15 20:13:21 -0500

    try again to gitignore

--- week10/quakes/main.js
+++ week10/quakes/main.js
@@ -2,13 +2,24 @@ import { getJSON, getLocation } from 'utilities.js';
 
 const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 function testGetQuakesForLocation() {
     // call the getLocation function to get the lat/long
-  
+    function success(pos) {
+        var crd = pos.coords;
+      
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+      }
+  getLocation(success);
     // use that information to build out the correct URL
     const geoUrl = baseUrl + // add location information here
     // use the url to request the correct quakes 
-  
+
     //log out the quakes for now.
+    console.log(geoURL);
   }
   getQuakesForLocation();
